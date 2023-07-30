bfad6fae6a0b0503b3851f5e8ed6ce24a3c6cc2d
kaisa1234 <39077560+kaisa1234@users.noreply.github.com>
2023-07-30 00:16:29 -0400

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/eduardo/Documentos/clase3/ws_clase3/src
+# Build directory: /home/eduardo/Documentos/clase3/ws_clase3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ejr2")
+subdirs("ejr3")
+subdirs("ejr4")
+subdirs("ejr5")
+subdirs("test1")
